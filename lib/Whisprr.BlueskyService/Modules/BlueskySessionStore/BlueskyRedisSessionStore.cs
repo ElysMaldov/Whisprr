278236ca36463c962a7db1624f073d23af94f7ab
Elys Maldov <maldovelys@gmail.com>
2026-02-24 10:13:09 +0700

    chore(BlueskyService): migrate from old project

---
+++ lib/Whisprr.BlueskyService/Modules/BlueskySessionStore/BlueskyRedisSessionStore.cs
@@ -0,0 +1,23 @@
+
+using StackExchange.Redis;
+using Whisprr.BlueskyService.Models.Domain;
+
+namespace Whisprr.BlueskyService.Modules.BlueskySessionStore;
+
+public class BlueskyRedisSessionStore(IDatabase db) : IBlueskySessionStore
+{
+  private string _sessionKey = "bluesky-session";
+
+  public async Task SaveSessionAsync(BlueskySession session)
+  {
+    var hash = session.ToHash();
+    await db.HashSetAsync(_sessionKey, hash);
+  }
+
+  public async Task<BlueskySession?> GetSessionAsync()
+  {
+    var hashFields = await db.HashGetAllAsync(_sessionKey);
+
+    return hashFields.ToBlueskySession();
+  }
+}
