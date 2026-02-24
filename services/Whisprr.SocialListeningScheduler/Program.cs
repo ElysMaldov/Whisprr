596406dd5d7fcfe6d21cdf551505a826377d1b96
Elys Maldov <maldovelys@gmail.com>
2026-02-24 13:12:42 +0700

    build(SocialListeningScheduler): setup project

---
+++ services/Whisprr.SocialListeningScheduler/Program.cs
@@ -0,0 +1,7 @@
+using Whisprr.SocialListeningScheduler;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
