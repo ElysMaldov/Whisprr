6275319913be3d07c253ed55f980de39dd3615b9
Elys Maldov <maldovelys@gmail.com>
2026-02-24 21:02:35 +0700

    build(SocialListeningScheduler): create TaskProgressStatus Pending migration

--- services/Whisprr.SocialListeningScheduler/Migrations/20260224135743_UpdateTaskProgressStatusWithPendingValue.Designer.cs
+++ services/Whisprr.SocialListeningScheduler/Migrations/20260224135743_UpdateTaskProgressStatusWithPendingValue.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Whisprr.Contracts.Enums;
@@ -12,9 +13,11 @@ using Whisprr.SocialListeningScheduler.Data;
 namespace Whisprr.SocialListeningScheduler.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260224135743_UpdateTaskProgressStatusWithPendingValue")]
+    partial class UpdateTaskProgressStatusWithPendingValue
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ services/Whisprr.SocialListeningScheduler/Migrations/20260224135743_UpdateTaskProgressStatusWithPendingValue.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Whisprr.SocialListeningScheduler.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTaskProgressStatusWithPendingValue : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:task_progress_status", "pending,processing,success,failed")
+                .OldAnnotation("Npgsql:Enum:task_progress_status", "processing,success,failed");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:task_progress_status", "processing,success,failed")
+                .OldAnnotation("Npgsql:Enum:task_progress_status", "pending,processing,success,failed");
+        }
+    }
+}
