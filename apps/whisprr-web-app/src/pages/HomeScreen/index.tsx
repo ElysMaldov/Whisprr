2e1b49752e89bc958d47da9da0cd70f766f0296b
Elys Maldov <maldovelys@gmail.com>
2026-02-25 21:36:22 +0700

    feat(WebApp): add Navbar to HomeScreen

--- apps/whisprr-web-app/src/pages/HomeScreen/index.tsx
+++ apps/whisprr-web-app/src/pages/HomeScreen/index.tsx
@@ -1,11 +1,12 @@
 import Hero from "@/components/Hero";
+import { Navbar1 } from "@/components/Navbar";
 
 export interface HomeScreenProps {}
 
 const HomeScreen = ({}: HomeScreenProps) => {
   return (
     <section className="flex flex-col w-full min-h-screen">
-      <nav></nav>
+      <Navbar1 className="w-full flex justify-center" />
       <Hero className="w-full flex items-center justify-center flex-1" />
     </section>
   );
