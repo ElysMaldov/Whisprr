10d4853f5e3eb81a9da3a3a4d96a658a052aded7
Elys Maldov <maldovelys@gmail.com>
2026-02-25 09:40:44 +0700

    feat(SocialScouter): implement StartSocialListeningTaskConsumer

--- services/Whisprr.SocialScouter/Modules/MessageBroker/Consumers/StartSocialListeningTaskConsumer.cs
+++ services/Whisprr.SocialScouter/Modules/MessageBroker/Consumers/StartSocialListeningTaskConsumer.cs
@@ -1,14 +1,49 @@
 
+using System.Threading.Channels;
 using MassTransit;
 using Whisprr.Contracts.Commands;
+using Whisprr.SocialScouter.Models;
 
 namespace Whisprr.SocialScouter.Modules.MessageBroker.Consumers;
 
-internal sealed class StartSocialListeningTaskConsumer : IConsumer<StartSocialListeningTask>
+internal sealed partial class StartSocialListeningTaskConsumer(
+  ILogger<StartSocialListeningTaskConsumer> logger,
+  ChannelWriter<SocialListeningTask> taskChannelWriter) : IConsumer<StartSocialListeningTask>
 {
   public async Task Consume(ConsumeContext<StartSocialListeningTask> context)
   {
-    Console.WriteLine($"New task! {context.Message.TaskId}");
-    // TODO handle
+    var taskContract = context.Message;
+
+    try
+    {
+      LogProcessingNewTask(logger, taskContract.TaskId);
+
+      // Map to domain
+      SocialListeningTask newTask = new()
+      {
+        Query = taskContract.Query,
+        CreatedAt = taskContract.CreatedAt,
+        Id = taskContract.TaskId,
+        SocialTopicId = taskContract.SocialTopicId,
+        SourcePlatformId = taskContract.SourcePlatformId,
+        UpdatedAt = DateTimeOffset.UtcNow
+      };
+
+      await taskChannelWriter.WriteAsync(newTask, context.CancellationToken);
+    }
+    catch (Exception ex)
+    {
+      LogProcessingTaskError(logger, ex, taskContract.TaskId);
+      throw;
+    }
   }
+
+  [LoggerMessage(
+      Level = LogLevel.Information,
+      Message = "Processing new social listening task {TaskId}")]
+  static partial void LogProcessingNewTask(ILogger<StartSocialListeningTaskConsumer> logger, Guid taskId);
+  [LoggerMessage(
+      Level = LogLevel.Error,
+      Message = "Failed to process task {TaskId}")]
+  static partial void LogProcessingTaskError(ILogger<StartSocialListeningTaskConsumer> logger, Exception ex, Guid taskId);
 }
